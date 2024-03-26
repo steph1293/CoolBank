bb6db2a3bf317008f1cdfcba654b29e5009ce811
Stephanie Diaz ocasio <stephanie.diaz@popular.com>
2024-03-26 16:45:47 -0400

    New Welcome Page for users after logging in

--- CoolBankBackend/authentication/urls.py
+++ CoolBankBackend/authentication/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from .views import CustomLoginView
+from .views import CustomWelcomeView
 
 urlpatterns = [
-    path("login/", CustomLoginView.as_view(), name="login"),
+    path("login/", CustomLoginView.as_view(), name="login"),   
+    path("welcome/", CustomWelcomeView.as_view(), name="welcome")
+
 ]
--- CoolBankBackend/authentication/views.py
+++ CoolBankBackend/authentication/views.py
@@ -1,4 +1,6 @@
+from pydoc import render_doc
 from django.contrib.auth.views import LoginView
+from django.views.generic import TemplateView  
 from django.urls import reverse_lazy
 
 
@@ -6,3 +8,7 @@ class CustomLoginView(LoginView):
     template_name = "login-custom.html"
     redirect_authenticated_user = True
     next_page = reverse_lazy("home")  # Assuming you have a home page named 'home'
+class CustomWelcomeView(TemplateView):
+    template_name = "welcome.html"
+    
+        
